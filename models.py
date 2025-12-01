17a9566e4c14b1f30adb73792cfca2b9d60fe67b
Fie <fie.bergli@hotmail.com>
2025-12-01 13:39:34 +0100

    one model accuracy

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from q1 import pad_batch
 import torch
 import torch.nn as nn
 
+
 class BaselineClassifier(nn.Module):
     """
     Baseline sequence classifier:
@@ -9,7 +10,14 @@ class BaselineClassifier(nn.Module):
     - Global pooling over time (mean / max / first)
     - Linear projection to num_classes
     """
-    def __init__(self, vocab_size:int, emb_dim: int = 300, num_classes: int = 2, pool: str = 'mean'):
+
+    def __init__(
+        self,
+        vocab_size: int,
+        emb_dim: int = 300,
+        num_classes: int = 2,
+        pool: str = "mean",
+    ):
         """
         pool: 'mean', 'max', or 'first'
         """
@@ -19,30 +27,23 @@ class BaselineClassifier(nn.Module):
         self.fc = nn.Linear(emb_dim, num_classes)
         self.pool = pool
 
+    def forward(self, x: torch.LongTensor) -> torch.Tensor:
+        # x: (batch, time), dtype=torch.long
+        # 1) embed -> (batch, time, emb)
+        x_emb = self.embedding(x)  # shape: (B, T, E)
 
-        def forward(self, x: torch.LongTensor) -> torch.Tensor:
-            # x: (batch, time), dtype=torch.long
-            # 1) embed -> (batch, time, emb)
-            x_emb = self.embedding(x) # shape: (B, T, E)
+        # 2) global pooling -> (batch, emb)
+        if self.pool == "mean":
+            # mean over time dimension, ignoring padding is more correct but here
+            # we assume padding indices have embeddings that don't bias too much.
+            out = x_emb.mean(dim=1)
+        elif self.pool == "max":
+            # max over time dimension
+            out, _ = x_emb.max(dim=1)
+        else:  # first
+            # take first token embedding (e.g., x_emb[:, 0, :])
+            out = x_emb[:, 0, :]
 
-            # 2) global pooling -> (batch, emb)
-            if self.pool == 'mean':
-                # mean over time dimension, ignoring padding is more correct but here
-                # we assume padding indices have embeddings that don't bias too much.
-                out = x_emb.mean(dim=1)
-            elif self.pool == 'max':
-                # max over time dimension
-                out, _ = x_emb.max(dim=1)
-            else: #first
-                # take first token embedding (e.g., x_emb[:, 0, :])
-                out = x_emb[:, 0, :]
-
-            # 3) linear projection -> (batch, num_classes)
-            output = self.fc(out)
-            return output
-
-
-
-
-
-
+        # 3) linear projection -> (batch, num_classes)
+        output = self.fc(out)
+        return output
