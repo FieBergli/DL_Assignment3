8d176d7c04b8e44669d53fda9eeb87d559f15ed7
Fie <fie.bergli@hotmail.com>
2025-12-01 11:48:49 +0100

    q1

--- q1.py
+++ q1.py
@@ -1,4 +1,6 @@
 from data import load_imdb
+from typing import List
+import torch
 
 (x_train, y_train), (x_val, y_val), (i2w, w2i), numcls = load_imdb(final=False)
 #print([i2w[w] for w in x_train[141]])
@@ -15,17 +17,18 @@ def pad_batch(batch_sequences: List[List[int]], pad_idx: int) -> torch.LongTenso
     Returns:
     Tensor of shape (batch_size, max_len) with dtype torch.long
     """
-    # 1) find the maximum length in this batch
+    #Find the maximum length in this batch
     max_len = max(len(seq) for seq in batch_sequences)
 
-    # 2) create a list of lists all equal length by padding with pad_idx
+    # Create a list of lists all equal length by padding with pad_idx
     padded = []
-    for seq in batch_sequences:
-    # copy the sequence and extend with pad_idx to max_len
-    padded_seq = seq + [pad_idx] * (max_len - len(seq))
-    padded.append(padded_seq)
 
-    # 3) convert to a torch tensor of dtype long
+    for seq in batch_sequences:
+        # copy the sequence and extend with pad_idx to max_len
+        padded_seq = seq + [pad_idx] * (max_len - len(seq))
+        padded.append(padded_seq)
+
+    # Convert to a torch tensor of dtype long
     batch_tensor = torch.tensor(padded, dtype=torch.long)
 
     return batch_tensor
