da6790cfed6b30a81b645cba81425139c02a4740
Fie <fie.bergli@hotmail.com>
2025-12-02 14:44:52 +0100

    q7

--- q6.py
+++ q6.py
@@ -2,7 +2,10 @@ import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import itertools
 from models import BaselineClassifier
+from q4 import train_epochs, iterate_batches, evaluate
+from data import load_imdb, load_imdb_synth, load_xor
 
 class MultiHeadSelfAttentionClassifier(BaselineClassifier):
     """
@@ -101,3 +104,56 @@ class MultiHeadSelfAttentionClassifier(BaselineClassifier):
         # 7) Classification layer from BaselineClassifier
         output = self.fc(out)                         # (B, num_classes)
         return output
+    
+def grid_search_attention(train_data, val_data, vocab_size, num_classes, pad_idx, num_epochs):
+    lrs = [1e-3, 5e-3, 1e-4]
+    batch_sizes = [32, 64, 128]
+    results = []
+
+    for lr, batch_size in itertools.product(lrs, batch_sizes):
+        print(f"\n=== Training with lr={lr}, batch={batch_size} ===")
+        model = MultiHeadSelfAttentionClassifier(vocab_size, emb=300, num_classes=num_classes, num_heads=6)
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+        train_loss, train_acc = train_epochs(model, train_data, batch_size, pad_idx, optimizer, num_epochs=num_epochs)
+        val_loss, val_acc = evaluate(model, val_data, batch_size, pad_idx)
+
+        print(f'lr={lr}, batch={batch_size} | train_acc={train_acc:.3f}, val_acc={val_acc:.3f}')
+        results.append((lr, batch_size, train_acc, val_acc))
+
+    return results
+
+(x_train_1, y_train_1), (x_val_1, y_val_1), (i2w_1, w2i_1), numcls_1 = load_imdb(final=False)
+train_data1 = (x_train_1, y_train_1)
+val_data1   = (x_val_1, y_val_1)
+
+(x_train_2, y_train_2), (x_val_2, y_val_2), (i2w_2, w2i_2), numcls_2 = load_imdb_synth()
+train_data2 = (x_train_2, y_train_2)
+val_data2   = (x_val_2, y_val_2)
+
+(x_train_3, y_train_3), (x_val_3, y_val_3), (i2w_3, w2i_3), numcls_3 = load_xor()
+train_data3 = (x_train_3, y_train_3)
+val_data3   = (x_val_3, y_val_3)
+
+pad_idx1 = w2i_1['.pad']
+pad_idx2 = w2i_2['.pad']
+pad_idx3 = w2i_3['.pad']
+
+# --- Run experiments with MultiHeadAttentionClassifier ---
+results1 = grid_search_attention(train_data1, val_data1,
+                                 vocab_size=len(i2w_1),
+                                 num_classes=numcls_1,
+                                 pad_idx=pad_idx1,
+                                 num_epochs=20)
+
+results2 = grid_search_attention(train_data2, val_data2,
+                                 vocab_size=len(i2w_2),
+                                 num_classes=numcls_2,
+                                 pad_idx=pad_idx2,
+                                 num_epochs=100)
+
+results3 = grid_search_attention(train_data3, val_data3,
+                                 vocab_size=len(i2w_3),
+                                 num_classes=numcls_3,
+                                 pad_idx=pad_idx3,
+                                 num_epochs=100)
